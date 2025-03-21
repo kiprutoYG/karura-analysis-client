32a124487245f49b95d9563066dfcbed6540e487
Elkana Kipruto <allanyegon443@gmail.com>
2025-03-21 17:21:01 +0300

    Window fix

--- app/components/map.js
+++ app/components/map.js
@@ -4,9 +4,9 @@ import React from "react";
 import { useState } from "react";
 import dynamic from "next/dynamic";
 import { useEffect } from "react";
-import { TileLayer } from "react-leaflet";
 
 const MapContainer = dynamic(() => import('react-leaflet').then((module) => module.MapContainer), {ssr: false});
+const TileLayer = dynamic(() => import('react-leaflet').then((module) => module.TileLayer), {ssr: false});
 
 
 export default function Map() {
